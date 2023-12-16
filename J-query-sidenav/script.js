36fc474a5692d268e8e6c76236bcd0f2c172df16
jen67 <amakrigift2000@gmail.com>
2023-12-16 23:42:18 +0100

    more codes

--- J-query-sidenav/script.js
+++ J-query-sidenav/script.js
@@ -1,24 +1,21 @@
 (function () {
+  "use strict";
 
-    "use strict";
+  const subMenus = document.querySelectorAll("ul li ul");
+  const menuLinks = document.querySelectorAll(".menulink");
 
-    
-    // Hide all submenus
+  // Hide all submenus
   function hideSubmenus() {
-    const subMenus = document.querySelectorAll("ul li ul");
     for (let i = 0; i < subMenus.length; i++) {
       subMenus[i].className = "hide-menu";
     }
   }
-hideSubmenus();
-  
-
-  const menuLinks = document.querySelectorAll(".menulink");
+  hideSubmenus();
 
   for (let i = 0; i < menuLinks.length; i++) {
     menuLinks[i].addEventListener("click", function (e) {
       e.preventDefault();
-     let thisMenu = this.parentNode.querySelector("ul");
+      let thisMenu = this.parentNode.querySelector("ul");
       if (thisMenu.classList.contains("hide-menu")) {
         hideSubmenus();
         thisMenu.className = "show-menu";
