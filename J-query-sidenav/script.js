b818c770f628652c6e3d73a56ac67368031ef877
jen67 <amakrigift2000@gmail.com>
2023-12-16 23:33:11 +0100

    more codes

--- J-query-sidenav/script.js
+++ J-query-sidenav/script.js
@@ -1,26 +1,25 @@
-const subMenus = document.querySelectorAll("ul li ul");
-
-hideSubmenus(); 
-
-function hideSubmenus() {
+(function () {
+  function hideSubmenus() {
+    const subMenus = document.querySelectorAll("ul li ul");
     for (var i = 0; i < subMenus.length; i++) {
       subMenus[i].className = "hide-menu";
     }
-}
+  }
 
-const menuLinks = document.querySelectorAll(".menulink");
+  hideSubmenus();
 
-for (var i = 0; i < menuLinks.length; i++) { 
-    menuLinks[i].addEventListener("click", function (e) { 
-        e.preventDefault();
-        thisMenu = this.parentNode.querySelector("ul");
-        if (thisMenu.classList.contains("hide-menu")) {
-            hideSubmenus();
-            thisMenu.className = "show-menu";
-        }
+  const menuLinks = document.querySelectorAll(".menulink");
 
-        else {
-            thisMenu.className = "hide-menu";
-        }
+  for (var i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].addEventListener("click", function (e) {
+      e.preventDefault();
+      thisMenu = this.parentNode.querySelector("ul");
+      if (thisMenu.classList.contains("hide-menu")) {
+        hideSubmenus();
+        thisMenu.className = "show-menu";
+      } else {
+        thisMenu.className = "hide-menu";
+      }
     });
-    }
+  }
+})();
