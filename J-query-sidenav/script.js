e4baa32012df44cf5bdc14f38e38dbbcb5a9e4be
jen67 <amakrigift2000@gmail.com>
2023-12-16 17:10:26 +0100

    added some codes

--- J-query-sidenav/script.js
+++ J-query-sidenav/script.js
@@ -3,7 +3,7 @@ const subMenus = document.querySelectorAll("ul li ul");
 for (var i = 0; i < subMenus.length; i++) {
     subMenus[i].className = "hide-menu";
 
-    subMenus[i].addEventListener("click", function (e) { 
-        e.classList.toggle("show-menu");  
-    });
+     subMenus[i].addEventListener("click", function (e) {
+       e.target.classList.toggle("show-menu");
+     });
 }
