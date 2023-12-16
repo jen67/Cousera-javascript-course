8d0c946a969845c360af822aee722d9094fbacf1
jen67 <amakrigift2000@gmail.com>
2023-12-16 21:02:05 +0100

    more codes

--- J-query-sidenav/script.js
+++ J-query-sidenav/script.js
@@ -3,3 +3,12 @@ const subMenus = document.querySelectorAll("ul li ul");
 for (var i = 0; i < subMenus.length; i++) {
     subMenus[i].className = "hide-menu";
 }
+
+const menuLinks = document.querySelectorAll(".menulink");
+
+for (var i = 0; i < menuLinks.length; i++) { 
+    menuLinks[i].addEventListener("click", function (e) { 
+        e.preventDefault();
+        alert("clicked")
+    });
+}
